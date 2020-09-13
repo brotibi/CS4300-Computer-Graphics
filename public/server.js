023b0713236d3bac08942624a5d9a5def8c97ee1
brotibi <brotibi@gmail.com>
2020-09-13 13:22:20 -0400

    Finished Assignment 1

---
+++ public/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+
+/*
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile('index.html');
+});*/
+
+
+app.listen(3000, '127.0.0.1');
