7ae577317e3712fea883467f449d1b03addd144c
brotibi <brotibi@gmail.com>
2020-09-29 22:50:32 -0400

    Finished Assignment 3

--- public/server.js
+++ public/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 
-/*
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+
+app.get('/assignment3.js', (req, res) => {
+    res.sendFile('assignment3.js');
 });
 
 app.get('/index.html', (req, res) => {
     res.sendFile('index.html');
-});*/
+});
 
 
 app.listen(3000, '127.0.0.1');
